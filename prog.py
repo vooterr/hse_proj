391a5b85e70ba296017965d13097dc1078127ee9
Votter <zigl.zigl535@gmail.com>
2024-09-18 22:29:34 +0300

    Test1

--- prog.py
+++ prog.py
@@ -1,2 +1,11 @@
 a = int(input("Введите целое число"))
 b = int(input("Введите второе целое число"))
+op = input("Введите +-*/")
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+else:
+    print(a / b)
